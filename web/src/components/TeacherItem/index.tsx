b92bad0cee079911d67bf980806c3da8104f629d
Oosasukel <rodrigo_gonn@hotmail.com>
2020-08-08 18:36:09 -0300

    connecting frontend to backend

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,79 +3,59 @@ import React from "react";
 import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
 
 import "./styles.css";
+import api from "../../services/api";
 
-function TeacherItem() {
-  return (
-    <div>
-      <article className="teacher-item">
-        <header>
-          <img
-            src="https://scontent.fsod9-1.fna.fbcdn.net/v/t1.0-9/104936079_679917595904173_2492582236253885495_n.jpg?_nc_cat=111&_nc_sid=8bfeb9&_nc_ohc=6hDF-3-kVe8AX_cCeSv&_nc_ht=scontent.fsod9-1.fna&oh=e241f3c1c31aff1d1ec93b582fdf08af&oe=5F532211"
-            alt="Carla Robles"
-          />
-          <div>
-            <strong>Carla Robles</strong>
-            <span>Biologia</span>
-          </div>
-        </header>
-
-        <p>
-          Desenvolve pesquisas nas mais diversas áreas da biologia e está sempre
-          disposta a plantar mais uma árvore.
-          <br />
-          <br />
-          Amante dos seres vivos.
-        </p>
-
-        <footer>
-          <p>
-            Preço/hora
-            <strong>R$ 4,90</strong>
-          </p>
-          <a href="https://wa.me/5515996976651" target="_blank">
-            <button type="button">
-              <img src={whatsappIcon} alt="Whatsapp" />
-              Entrar em contato
-            </button>
-          </a>
-        </footer>
-      </article>
-
-      <article className="teacher-item">
-        <header>
-          <img
-            src="https://avatars2.githubusercontent.com/u/58633356?s=460&u=cff380187b6ae342bddd67343967f63ec7a45fe0&v=4"
-            alt="Rodrigo Gonçalves"
-          />
-          <div>
-            <strong>Rodrigo Gonçalves</strong>
-            <span>Estrutura de Dados II</span>
-          </div>
-        </header>
-
-        <p>
-          Entusiasta das melhores tecnologias de estrutura de dados avançadas.
-          <br />
-          <br />
-          Apaixonado por programação.
-        </p>
-
-        <footer>
-          <p>
-            Preço/hora
-            <strong>R$ 75,00</strong>
-          </p>
-
-          <a href="https://wa.me/5515997332834" target="_blank">
-            <button type="button">
-              <img src={whatsappIcon} alt="Whatsapp" />
-              Entrar em contato
-            </button>
-          </a>
-        </footer>
-      </article>
-    </div>
-  );
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
 }
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post("connections", {
+      user_id: teacher.id,
+    });
+  }
+
+  return (
+    <article className="teacher-item">
+      <header>
+        <img src={teacher.avatar} alt={teacher.name} />
+        <div>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
+        </div>
+      </header>
+
+      <p>{teacher.bio}</p>
+
+      <footer>
+        <p>
+          Preço/hora
+          <strong>R$ {teacher.cost}</strong>
+        </p>
+        <a
+          onClick={createNewConnection}
+          href={`https://wa.me/${teacher.whatsapp}`}
+          target="_blank"
+        >
+          <button type="button">
+            <img src={whatsappIcon} alt="Whatsapp" />
+            Entrar em contato
+          </button>
+        </a>
+      </footer>
+    </article>
+  );
+};
 
 export default TeacherItem;
